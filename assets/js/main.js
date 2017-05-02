d9caff72400497b82b2f58b6807fed091e5c8752
Blanca Pérez <blankp@gmail.com>
2017-05-02 13:18:31 -0300

    added items to ul form json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-alert("Hola Mundo");
+var tareas = arr.slice(0,10);
+
+var miUl = document.getElementById("tareas");
+var liAux, textAux;
+tareas.forEach(function(el){
+	liAux = document.createElement("li");
+	textAux = document.createTextNode(el.title);
+	liAux.appendChild(textAux); 
+  miUl.appendChild(liAux);
+});
+
